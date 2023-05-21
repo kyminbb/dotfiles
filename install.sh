cef6defbe237707cedfcaf027f2e3bac8f355247
Kyungmin Kim <kyminbb.kim@gmail.com>
2023-05-21 00:02:57 -0400

    Add `oh-my-zsh` plugin installation

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 readonly NC="\033[0m"
+readonly GREEN="\033[0;32m"
+readonly BLUE="\033[0;34m"
 readonly BOLD_GREEN="\033[1;32m"
 readonly BOLD_WHITE="\033[1;37m"
 
 prompt() {
-  echo -e "${BOLD_GREEN}==>${BOLD_WHITE} $1${NC}"
+  echo -e "${2:-$GREEN}==>$BOLD_WHITE $1$NC"
 }
 
 brew_install() {
@@ -23,23 +25,24 @@ init_utils() {
 init_zsh() {
   # Init oh-my-zsh
   prompt "Installing ${BOLD_GREEN}oh-my-zsh"
-  if [[ ! -d $HOME/.oh-my-zsh ]]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  fi
+  [[ -d $HOME/.oh-my-zsh ]] || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  prompt "Installing zsh-autosuggestions" "$BLUE"
+  local target_path="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+  [[ -d $target_path ]] || git clone https://github.com/zsh-users/zsh-autosuggestions "$target_path"
+  prompt "Installing zsh-syntax-highlighting" "$BLUE"
+  brew_install zsh-syntax-highlighting
 
-  # Init Starship
+  # Init starship
   prompt "Installing ${BOLD_GREEN}starship"
   brew_install starship
-  prompt "Installing ${BOLD_GREEN}font-lilex-nerd-font"
+  prompt "Installing font-lilex-nerd-font" "$BLUE"
   brew tap homebrew/cask-fonts && brew_install font-lilex-nerd-font
   cp zsh/starship.toml ~/.config/starship.toml
 
   # Init alacritty
   prompt "Installing ${BOLD_GREEN}alacritty"
   brew_install alacritty
-  if [[ -n $(infocmp alacritty >/dev/null 2>&1) ]]; then
-    sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
-  fi
+  [[ -z $(infocmp alacritty >/dev/null 2>&1) ]] || sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
   cp -R alacritty ~/.config
 
   # Init vim
