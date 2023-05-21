13946865c4553147bc173681092ad27518e4d84d
Kyungmin Kim <kyminbb.kim@gmail.com>
2023-05-20 23:23:06 -0400

    Stylize installation CLI

--- install.sh
+++ install.sh
@@ -1,53 +1,56 @@
 #!/bin/bash
 
+readonly NC="\033[0m"
+readonly BOLD_GREEN="\033[1;32m"
+readonly BOLD_WHITE="\033[1;37m"
+
+prompt() {
+  echo -e "${BOLD_GREEN}==>${BOLD_WHITE} $1${NC}"
+}
+
 brew_install() {
   brew list "$1" >/dev/null || brew install "$1"
 }
 
 init_utils() {
   declare -ar formulae=(exa colima tmux)
-  echo "Initializing utils [${formulae[*]}]..."
   for formula in "${formulae[@]}"; do
+    prompt "Installing ${BOLD_GREEN}${formula}"
     brew_install "$formula"
   done
-  echo -e "DONE!\n"
 }
 
 init_zsh() {
   # Init oh-my-zsh
-  echo "Initializing oh-my-zsh..."
+  prompt "Installing ${BOLD_GREEN}oh-my-zsh"
   if [[ ! -d $HOME/.oh-my-zsh ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   fi
-  echo -e "DONE!\n"
 
   # Init Starship
-  echo "Initializing starship..."
+  prompt "Installing ${BOLD_GREEN}starship"
   brew_install starship
+  prompt "Installing ${BOLD_GREEN}font-lilex-nerd-font"
   brew tap homebrew/cask-fonts && brew_install font-lilex-nerd-font
   cp zsh/starship.toml ~/.config/starship.toml
-  echo -e "DONE!\n"
 
   # Init alacritty
-  echo "Initializing alacritty..."
+  prompt "Installing ${BOLD_GREEN}alacritty"
   brew_install alacritty
   if [[ -n $(infocmp alacritty >/dev/null 2>&1) ]]; then
     sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
   fi
   cp -R alacritty ~/.config
-  echo -e "DONE!\n"
 
   # Init vim
-  echo "Initializing vim..."
+  prompt "Initializing ${BOLD_GREEN}vim"
   cp vim/.vimrc ~/.vimrc
-  echo -e "DONE!\n"
 
   # Init zsh
-  echo "Initializing zsh..."
+  prompt "Initializing ${BOLD_GREEN}zsh"
   cp zsh/.zshrc ~/.zshrc
-  echo "DONE!"
 }
 
 init_utils
 init_zsh
-echo "Run \`source ~/.zshrc\` to apply changes!"
+echo -e "\nRun \`source ~/.zshrc\` to apply changes!"
